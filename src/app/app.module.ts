ce647c37a0c3cb4adda4e4180dedb75d61e806f7
Arulsaru <saruarul154@gmail.com>
2025-03-08 16:29:29 +0530

    About section fully updated with icons

---
+++ src/app/app.module.ts
@@ -0,0 +1,6 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  imports: [FontAwesomeModule],
+})
+export class AppModule {}
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faAngular,
+  faJs,
+  faNodeJs,
+  faDocker,
+  faReact,
+  faPython,
+  faPhp,
+  faGit,
+  faAws,
+  // faDatabase,
+  faCss3Alt,
+  faJava,
+  faGithub,
+  // faMongoDB,
+  faJenkins,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  faAngular = faAngular;
+  faJs = faJs;
+  faNodeJs = faNodeJs;
+  faDocker = faDocker;
+  faReact = faReact;
+  faPython = faPython;
+  faPhp = faPhp;
+  faGit = faGit;
+  faAws = faAws;
+  // faDatabase = faDatabase;
+  faCss = faCss3Alt;
+  faJava = faJava;
+  faGithub = faGithub;
+  faJenkins = faJenkins;
 }
